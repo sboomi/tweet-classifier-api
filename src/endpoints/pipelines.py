7fec9de1b34e442a64e18b5a4c027a4f89dd55b2
Shadi Boomi <shadi.boomi@gmail.com>
2021-05-04 16:34:24 +0200

    remove priv files

--- src/endpoints/pipelines.py
+++ src/endpoints/pipelines.py
@@ -1,3 +1,8 @@
 from flask import Blueprint, jsonify, request
 
 pipelines = Blueprint(name="pipelines", import_name=__name__)
+
+
+@pipelines.route('/new', methods=['POST'])
+def new_pipeline():
+    pass
